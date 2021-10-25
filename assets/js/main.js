b88835d88b946b84438bdd0144ba753f3def3f71
Mattia311 <mattia.space98@gmail.com>
2021-10-25 15:03:15 +0200

    struttura eser

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const post = [
+    {
+        userName: 'Phil Mangione',
+        data_creazione: '4 mesi fa',
+        img_profil: 'https://images.unsplash.com/photo-1635148977968-d1c32cd614b4?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+        testo: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam est asperiores voluptate excepturi reprehenderit molestias perspiciatis tempora suscipit temporibus provident minus, enim, quaerat animi? Qui hic incidunt obcaecati neque praesentium!',
+        img: "https://images.unsplash.com/photo-1593642531955-b62e17bdaa9c?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        like: 80,
+
+    },
+    {
+    
+    },
+    {
+
+    }
+]
